1357839411099c35142043583df872d0fb4a5f16
keenanNS <keenan.simpson199@gmail.com>
2020-09-07 18:29:27 -0400

    KS - simulation verking, needs constants

--- pull_simulation/sim.py
+++ pull_simulation/sim.py
@@ -3,10 +3,10 @@
 import random
 import sys
 from simple_pid import PID
+import matplotlib.pyplot as plt
+import numpy as np
 
 print('MH3 Autopull PID learning simulation')
-
-#this should output a distance travelled. I think it will be easier if we try to out put velocity/
 
 # our input is displacement. d = f(pressure) where the function is some PID control
 
@@ -27,11 +27,15 @@ print('MH3 Autopull PID learning simulation')
 P = int(sys.argv[1])
 I = int(sys.argv[2])
 D = int(sys.argv[3])
+PRINT = int(sys.argv[4])
 
-WEIGHT = 1000 * 4.44822#TODO weight of tractor
+WEIGHT = 1600 * 4.44822 #TODO weight of tractor
 MASS = WEIGHT / 9.8
 VARIATION = 5 #TODO this is the amount that we thing the pressure will vary, (how much it could jump / time step)
 MAX_PRESSURE = 4000 #psi or some other value
+REDUCTION = 1
+AREA = 1
+#PRINT = False
 
 class Pull:
 	def __init__(self, P, I, D):
@@ -40,81 +44,106 @@ class Pull:
 		self.D = D
 
 	def Distance(self, velocities, time):
-		avg_velocity = sum(velocities)/len(velocities)
+		avg_velocity = sum(velocities) / len(velocities)
+		if PRINT:
+			print("distance ", avg_velocity * time)
 		return avg_velocity * time
 
-	def Net_Force(self, Load, Pressure_in, done):
+	def Net_Force(self, Load, Pressure_in): # should be net work
 		#TODO calculate overall reduction
-		reduction = 0.69
-		pull = Pressure_in * reduction 
+		pull = Pressure_in * REDUCTION * AREA 
 
-		if pull <= 0:
+		net_force = pull - Load
+
+		if PRINT:
+			print("net_force ", net_force)
+
+		return net_force
+
+	def Velocity(self, net_force, prev_velocity,done):
+		# Work_tractor - Work_sled = (1/2) * (mass_tractor)(Velocity^2 - prev_velocity^2)
+		if (2 * net_force / MASS + (prev_velocity**2)) <= 0:
+			return 0, True
+
+		velocity = np.sqrt(2 * net_force / MASS + (prev_velocity**2))
+
+		if PRINT:
+			print("velocity", velocity)
+
+		if velocity <= 0:
 			done = True
 
-		if not done:
-			return pull - Load, done
-		else: 
-			sys.exit("it is complete")
+		return velocity, done
 
-	def Velocity(self, net_force, prev_velocity):
-		acceleration = net_force / MASS # mass is the mass of the tractor because mass of sled is accounted for in net_force
-		velocity = prev_velocity + (acceleration)
-		#velocity = (prev_velocity * prev_net_force) / net_force #conservation of momentum
-		print("velocity: " ,velocity)
-		velocities.append(velocity)
-		return velocity
-
-	def Load(self, Distance):
-		return (Distance * 10) + 100 # TODO rate of change in load / unit of measurement (meters). 1000 represents inertia of sled on wheels
+	def Load(self, Distance): # Work of sled because * 1 second
+		if PRINT:
+			print("load ", Distance * 10 +2000)
+		load = Distance * 10 +2000
+		return load # TODO rate of change in load / unit of measurement (meters). 1000 represents inertia of sled on wheels
 
 	def Measured_Pressure(self, Net_Force):
-		pressure = Net_Force / 0.69 #TODO this is the inverse of the reduction 
+		pressure = 3999 #TODO this is the inverse of the reduction 
+		if PRINT:
+			print("pressure ", pressure)
 		return random.triangular(pressure - VARIATION, pressure + VARIATION, pressure) # (min, max, mode) prob distribution
 
 	def Presure_in(self, Pressure): #controlled by displacement, equivalent of applied force
 		pid = PID(self.P, self.I, self.D, setpoint = 4000, output_limits = (3900, 4000))
 		control = pid(Pressure)
-		print("control" ,control)
+		if PRINT:
+			print ("control ", control)
 		
-		return control #this needs to get converted to new pressure
-
-
-
-#pull = Pull(#TODO implement command line args)
-
-print("still needs implementing")
-print(P, I, D)
+		return control #this needs to get converted to new pressure it should be force before reduction
 
 pull = Pull(P,I,D)
 
 time = 0
 prev_velocity = 0
 distance = 0
-prev_net_force = 1
 done = False
 pressure_in = 4000
 Measured_Pressure = 3000
 velocities = []
-while not done:
-	load = pull.Load(distance)
-	print("load", load)
-	pressure_in = pull.Presure_in(Measured_Pressure)
-	print("applied pressure", pressure_in)
-	net_force, done = pull.Net_Force(load, pressure_in, done)
-	print("net_force", net_force)
+loads = []
+net_forces = []
+prev_net_force = 1000
 
-	velocity = pull.Velocity(net_force, prev_velocity)
-	distance = pull.Distance(velocities, time)
-	Measured_Pressure = pull.Measured_Pressure(net_force)
-	print("distance",distance)
-	time +=1
-	if done:
-		sys.exit("it is complete")
-	# if time > 100 or done:
-	# 	sys.exit()
+while not done:
+	if PRINT:
+		print("step ***********************")
 	
 
-print("distance travelled is ", distance)
+	load = pull.Load(distance)
+	loads.append(load)
+	pressure_in = pull.Presure_in(Measured_Pressure)
+	net_force= pull.Net_Force(load, pressure_in)
+	net_forces.append(net_force)
+	velocity, done = pull.Velocity(net_force, prev_velocity,done)
+	velocities.append(velocity)
+	distance = pull.Distance(velocities, time)
+	Measured_Pressure = pull.Measured_Pressure(net_force)
+	prev_net_force = net_force
+	prev_velocity = velocity
+	time += 1
+
+	if time == 200:
+		break
+	if done:
+		print("steps" , time)
+		break
+
+if not PRINT:
+	print("distance travelled is ", distance, "time elapsed is ", time)
+	Time = list(range(time))
+	plt.plot(Time, loads)
+	plt.ylabel('load')
+	plt.show()
+	plt.plot(Time, net_forces)
+	plt.ylabel('net_force')
+	plt.show()
+	plt.plot(Time, velocities)
+	plt.ylabel('velocity')
+	plt.show()
 
 
 
