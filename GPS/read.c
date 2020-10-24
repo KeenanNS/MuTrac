371fb8f64239e6f823f1d5a0c3c535702fcf6a4b
KeenanNS <keenan.simpson1999@gmail.com>
2020-10-24 19:21:23 -0400

    KS need on host

--- GPS/read.c
+++ GPS/read.c
@@ -5,12 +5,23 @@
 #include <math.h>
 #include <errno.h>
 
-int main() {
+int main(void) {
 struct timeval tv;
 struct gps_data_t gps_data;
+char str[50];
+
+FILE *fp = fopen("gps_data.txt", "w");
+if (fp == NULL){
+printf("couldn't open file for GPS data");
+return EXIT_FAILURE;
+}
 
 if ((gps_open("localhost", "2947", &gps_data)) == -1) {
-    printf("code: %d, reason: %s\n", errno, gps_errstr(errno));
+//fflush(stdout);
+    sprintf(str, "code: %d, reason: %s\n", errno, gps_errstr(errno));
+    printf(str);
+fwrite(str, 1, sizeof(str), fp);
+    fclose(fp);
     return EXIT_FAILURE;
 }
 gps_stream(&gps_data, WATCH_ENABLE | WATCH_JSON, NULL);
@@ -19,8 +30,10 @@ while (1) {
     /* wait for 2 seconds to receive data */
     if (gps_waiting (&gps_data, 2000000)) {
         /* read data */
-        if ((gps_read(&gps_data,NULL,0)) == -1) {
-            printf("error occured reading gps data. code: %d, reason: %s\n", errno, gps_errstr(errno));
+        if ((gps_read(&gps_data)) == -1) {
+		//fflush(stdout);
+	    sprintf(str, "error occured reading gps data. code: %d, reason: %s\n", errno, gps_errstr(errno));
+            fwrite(str, 1, sizeof(str), fp);
         } else {
             /* Display data from the GPS receiver. */
             if ((gps_data.status == STATUS_FIX) && 
@@ -28,14 +41,20 @@ while (1) {
                 !isnan(gps_data.fix.latitude) && 
                 !isnan(gps_data.fix.longitude)) {
                     //gettimeofday(&tv, NULL); EDIT: tv.tv_sec isn't actually the timestamp!
-                    printf("latitude: %f, longitude: %f, speed: %f, timestamp: %lf\n", gps_data.fix.latitude, gps_data.fix.longitude, gps_data.fix.speed, gps_data.fix.time); //EDIT: Replaced tv.tv_sec with gps_data.fix.time
+		//fflush(stdout);
+printf(str);	        
+sprintf(str, "%f,%f,%f,%lf", gps_data.fix.latitude, gps_data.fix.longitude, gps_data.fix.speed, gps_data.fix.time); 
+		fwrite(str, 1, sizeof(str), fp);
             } else {
-                printf("no GPS data available\n");
+		//fflush(stdout);
+printf(str);		
+sprintf(str,"No GPS data available");
+                fwrite(str,1,sizeof(str), fp);
             }
         }
     }
 
-    sleep(3);
+    sleep(1);
 }
 
 /* When you are done... */
