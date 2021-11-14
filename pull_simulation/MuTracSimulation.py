a13f3ba307524a6bd212f519ad7a296592151a32
keenanNS <keenan.simpson199@gmail.com>
2021-11-14 15:52:48 -0800

    update

--- pull_simulation/MuTracSimulation.py
+++ pull_simulation/MuTracSimulation.py
@@ -6,7 +6,7 @@ import math
 
 GRAVITY = 10 # lol
 BASE_RATIO = 11.7 # engine rpm to motor rpm
-MAX_DISPLACEMENT = 0.009 # flow rate of oil in m^3 you need to find this constant
+MAX_DISPLACEMENT = 0.009 # flow rate of oil in m^3 
 TIRE_DIAMETER = 1 # meter this is also wrong
 
 
@@ -27,6 +27,7 @@ class Tractor:
 		return self.mass * GRAVITY
 
 	def get_rpms(self):
+		#TODO this should be a function of load etc
 		return 3600.0
 		#this is new work, idk how to do this. You probably need to look at documents or run an experiment
 
@@ -39,7 +40,7 @@ class Tractor:
 	def get_requested_power(self, theta, load):
 		return self.get_rpms() * self.get_displacement(theta) * load
 
-	def get_distance_from_displacement(self):
+	def get_distance_from_displacement(self): # this is wheere you'll determine your time base 
 		return self.displacement * BASE_RATIO * TIRE_DIAMETER
 
 
@@ -54,10 +55,9 @@ class PullingSurface:
 		return 0.4 # same as above but for the sled plate
 
 class Sled:
-	def __init__(self, rate_of_increase, starting_weight, max_weight):
+	def __init__(self, rate_of_increase, starting_weight):
 		self.rate_of_increase = rate_of_increase
 		self.starting_weight = starting_weight
-		self.max_weight = max_weight
 
 	def get_current_load(self, current_position):
 		return self.starting_weight + self.rate_of_increase * current_position # load increases per meter this is wrong it should be based on friction
@@ -69,14 +69,15 @@ def get_net_force():
 
 # at each time step you need to determine the load of the sled 
 
-MH2 = Tractor(1,1,1)
-sled = Sled(100, 10000, 100000000)
+MH2 = Tractor(1,1,1) # this is where the __init__ method gets called
+sled = Sled(100, 10000)
 
 current_position = 0
 
+# for now, have this run and output a reasonable number ~ 100 meters, but talk to shamus and john about it. Find the all the constants you need (max displacement etc. )
 while(1):
 	sled_load = sled.get_current_load(current_position)
-	print(MH2.get_requested_power(1, sled_load),MH2.max_available_power(1))
+	#rint(MH2.get_requested_power(1, sled_load),MH2.max_available_power(1))
 	if(MH2.get_requested_power(1, sled_load) < MH2.max_available_power(1)):
 		current_position += MH2.get_distance_from_displacement()
 	else:
